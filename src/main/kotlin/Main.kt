133cbac4bb70eec6a085e9ed9ab850d8f58c6542
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2022-12-01 13:58:17 +0000

    Add Build workflow (#13)
    * Create Build workflow
    
    * Add execution permission to gradlew
    
    * Add Detekt suggestions
    
    * Add Detekt config

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,9 @@ import org.koin.core.context.GlobalContext.startKoin
 import org.koin.ksp.generated.defaultModule
 
 suspend fun main() {
-
     startKoin {
         defaultModule()
     }
 
     Application().main()
-
 }
