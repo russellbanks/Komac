68e87dee89da863e1cce0927b1f314e4de2aded1
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-01-08 20:36:56 +0000

    Use Windows Token Store over Windows Credentials

--- src/main/kotlin/token/TokenStore.kt
+++ src/main/kotlin/token/TokenStore.kt
@@ -2,28 +2,29 @@ package token
 
 import com.github.ajalt.mordant.rendering.TextColors.brightGreen
 import com.github.ajalt.mordant.rendering.TextColors.brightWhite
+import com.github.ajalt.mordant.rendering.TextColors.red
 import com.github.ajalt.mordant.terminal.Terminal
-import com.microsoft.alm.secret.Credential
-import com.microsoft.alm.storage.windows.CredManagerBackedCredentialStore
+import com.microsoft.alm.secret.Token
+import com.microsoft.alm.secret.TokenType
+import com.microsoft.alm.storage.windows.CredManagerBackedTokenStore
 import org.kohsuke.github.GitHubBuilder
 import org.koin.core.annotation.Single
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.get
 import java.io.IOException
 
 @Single
-class TokenStore : KoinComponent {
+class TokenStore {
     lateinit var token: String
     fun getToken(terminal: Terminal) {
-        if (System.getProperty(osName).startsWith(windows)) {
-            val credentialStore = CredManagerBackedCredentialStore()
+        val operatingSystem = System.getProperty(osName)
+        if (operatingSystem.startsWith(windows)) {
+            val credentialStore = CredManagerBackedTokenStore()
             if (credentialStore[credentialKey] == null) {
                 terminal.promptForToken().also {
-                    credentialStore.add(credentialKey, Credential(credentialUsername, it))
+                    credentialStore.add(credentialKey, Token(it, TokenType.Personal))
                     token = it
                 }
             } else {
-                token = credentialStore[credentialKey].Password
+                token = credentialStore[credentialKey].Value
             }
         }
     }
@@ -34,10 +35,10 @@ class TokenStore : KoinComponent {
             println(brightGreen("Please enter your GitHub personal access token:"))
             token = prompt(brightWhite("Token"))
             val isValid = try {
-                GitHubBuilder().withOAuthToken(get<TokenStore>().token).build()
+                GitHubBuilder().withOAuthToken(token).build()
                 true
             } catch (_: IOException) {
-                println(brightGreen("Invalid token. Please try again."))
+                println(red("Invalid token. Please try again."))
                 false
             }
         } while (token == null || !isValid)
