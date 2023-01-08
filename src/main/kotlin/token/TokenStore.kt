198277141ff920b56b76666fe4a86733712ffeda
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-01-08 20:48:06 +0000

    Add Credential Token Storage for Linux and macOS

--- src/main/kotlin/token/TokenStore.kt
+++ src/main/kotlin/token/TokenStore.kt
@@ -6,6 +6,9 @@ import com.github.ajalt.mordant.rendering.TextColors.red
 import com.github.ajalt.mordant.terminal.Terminal
 import com.microsoft.alm.secret.Token
 import com.microsoft.alm.secret.TokenType
+import com.microsoft.alm.storage.SecretStore
+import com.microsoft.alm.storage.macosx.KeychainSecurityBackedTokenStore
+import com.microsoft.alm.storage.posix.GnomeKeyringBackedTokenStore
 import com.microsoft.alm.storage.windows.CredManagerBackedTokenStore
 import org.kohsuke.github.GitHubBuilder
 import org.koin.core.annotation.Single
@@ -15,17 +18,24 @@ import java.io.IOException
 class TokenStore {
     lateinit var token: String
     fun getToken(terminal: Terminal) {
-        val operatingSystem = System.getProperty(osName)
-        if (operatingSystem.startsWith(windows)) {
-            val credentialStore = CredManagerBackedTokenStore()
-            if (credentialStore[credentialKey] == null) {
-                terminal.promptForToken().also {
-                    credentialStore.add(credentialKey, Token(it, TokenType.Personal))
-                    token = it
-                }
-            } else {
-                token = credentialStore[credentialKey].Value
+        val credentialStore = getCredentialStore()
+        if (credentialStore[credentialKey] == null) {
+            terminal.promptForToken().also {
+                credentialStore.add(credentialKey, Token(it, TokenType.Personal))
+                token = it
             }
+        } else {
+            token = credentialStore[credentialKey].Value
+        }
+    }
+
+    private fun getCredentialStore(): SecretStore<Token> {
+        val operatingSystem = System.getProperty(osName)
+        return when {
+            operatingSystem.startsWith(windows) -> CredManagerBackedTokenStore()
+            operatingSystem.startsWith(linux) -> GnomeKeyringBackedTokenStore()
+            operatingSystem.startsWith(mac) -> KeychainSecurityBackedTokenStore()
+            else -> throw IOException("Unsupported operating system")
         }
     }
 
@@ -48,6 +58,8 @@ class TokenStore {
     companion object {
         private const val osName = "os.name"
         private const val windows = "Windows"
+        private const val linux = "Linux"
+        private const val mac = "Mac"
         private const val credentialUsername = "github-access-token"
         private const val credentialKey = "komac/$credentialUsername"
     }
