3b8a10ee784e5e3dc10cc0594cadf82856a0de74
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-02-19 12:40:32 +0000

    feat: flag to skip token remove prompt
    Resolves #85

--- src/main/kotlin/commands/token/Remove.kt
+++ src/main/kotlin/commands/token/Remove.kt
@@ -1,6 +1,8 @@
 package commands.token
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
 import kotlinx.coroutines.runBlocking
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -8,9 +10,10 @@ import token.TokenStore
 
 class Remove : CliktCommand(), KoinComponent {
     private val tokenStore: TokenStore by inject()
+    private val skipPrompt: Boolean by option("-y", "--yes").flag(default = false)
 
     override fun run() = runBlocking {
-        val shouldDelete = confirm(
+        val shouldDelete = if (skipPrompt) true else confirm(
             text = currentContext.terminal.colors.warning("Would you like to remove the currently stored token?")
         )
         if (shouldDelete == true) {
