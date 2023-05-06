b1474e12ddb09a7d2e4a6e2a635708b7956f872c
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-05-06 14:14:59 +0100

    feat: add message for number of branches deleted

--- src/main/kotlin/commands/Cleanup.kt
+++ src/main/kotlin/commands/Cleanup.kt
@@ -5,16 +5,33 @@ import data.GitHubImpl
 
 class Cleanup : CliktCommand(name = "cleanup") {
     override fun run() {
-        val fork = GitHubImpl.getWingetPkgsFork(currentContext.terminal)
-        fork.branches.values
+        val wingetPkgsFork = GitHubImpl.getWingetPkgsFork(currentContext.terminal)
+        var branchesDeleted = 0
+
+        wingetPkgsFork.branches.values
             .filterNot { it.name == GitHubImpl.microsoftWinGetPkgs.defaultBranch }
             .forEach { branch ->
                 runCatching {
-                    GitHubImpl.microsoftWinGetPkgs.queryCommits().from(branch.shA1).list().withPageSize(1).first().let {
-                        warning("Deleting ${branch.name} because it was merged in ${it.htmlUrl}")
-                        fork.getRef("heads/${branch.name}").delete()
+                    val commit = GitHubImpl.microsoftWinGetPkgs
+                        .queryCommits()
+                        .from(branch.shA1)
+                        .list()
+                        .withPageSize(1)
+                        .first()
+
+                    commit.let {
+                        val branchName = branch.name
+                        warning("Deleting $branchName because it was merged in ${it.htmlUrl}")
+                        wingetPkgsFork.getRef("heads/$branchName").delete()
+                        branchesDeleted++
                     }
                 }
             }
+
+        if (branchesDeleted == 0) {
+            echo("No branches were found that could be deleted")
+        } else {
+            success("$branchesDeleted branches were deleted")
+        }
     }
 }
