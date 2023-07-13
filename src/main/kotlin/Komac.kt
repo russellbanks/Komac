95046eefb4f4adabf597deea6beda28c2299dfac
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-07-13 16:19:52 +0100

    Add `--use-system-proxy` parameter

--- src/main/kotlin/Komac.kt
+++ src/main/kotlin/Komac.kt
@@ -1,11 +1,26 @@
 
-import com.github.ajalt.clikt.core.NoOpCliktCommand
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import network.Proxy
 
-class Komac : NoOpCliktCommand(printHelpOnEmptyArgs = true) {
+class Komac : CliktCommand(printHelpOnEmptyArgs = true) {
+    private val useSystemProxy: Boolean by option(
+        "--use-system-proxy",
+        help = "When enabled, Komac will use the system's proxy",
+        envvar = "USE_SYSTEM_PROXY"
+    ).flag(default = false)
+
     override fun aliases(): Map<String, List<String>> = mapOf(
         "up" to listOf("update"),
         "rm" to listOf("remove"),
         "delete" to listOf("remove"),
         "cleanup" to listOf("branch", "cleanup")
     )
+
+    override fun run() {
+        if (useSystemProxy) {
+            Proxy.useSystemProxy()
+        }
+    }
 }
