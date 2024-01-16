8e4c6aec3c84327823ac995182a46e8fa7c243df
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2024-01-16 14:29:30 +0000

    Create deb packages for Linux

--- src/exe/utils.rs
+++ src/exe/utils.rs
@@ -1,6 +1,7 @@
 use byteorder::{LittleEndian, ReadBytesExt};
 use color_eyre::eyre::Result;
 use std::io::Cursor;
+use std::mem;
 
 pub const fn align(value: usize, boundary: usize) -> usize {
     if value % boundary == 0 {
@@ -13,7 +14,7 @@ pub const fn align(value: usize, boundary: usize) -> usize {
 pub fn get_widestring_size(data: &[u8], offset: usize) -> Result<usize> {
     let mut cursor = Cursor::new(data);
     let mut index = offset;
-    for i in (index..data.len()).step_by(2) {
+    for i in (index..data.len()).step_by(mem::size_of::<u16>()) {
         cursor.set_position(i as u64);
 
         if cursor.read_u16::<LittleEndian>()? == 0 {
