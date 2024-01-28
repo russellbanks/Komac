14778908773e74b50ace2c7a25e0549a13be98dc
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2024-01-28 19:21:42 +0000

    Replace language-tags with oxilangtag
    oxilangtag is much more lightweight as language-tags contains a language tag database which Komac doesn't need. We just want to validate the language tags.

--- src/types/language_tag.rs
+++ src/types/language_tag.rs
@@ -1,25 +1,24 @@
 use crate::prompts::prompt::RequiredPrompt;
 use derive_more::{AsRef, Deref, Display, FromStr};
 use serde::{Deserialize, Serialize};
-use std::cmp::Ordering;
 use std::str::FromStr;
 
 #[derive(
-    AsRef, Clone, Debug, Deref, Display, Deserialize, Serialize, FromStr, Eq, PartialEq, Hash,
+    AsRef,
+    Clone,
+    Debug,
+    Deref,
+    Display,
+    Deserialize,
+    Serialize,
+    FromStr,
+    Eq,
+    PartialEq,
+    Hash,
+    Ord,
+    PartialOrd,
 )]
-pub struct LanguageTag(language_tags::LanguageTag);
-
-impl PartialOrd for LanguageTag {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for LanguageTag {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.as_str().cmp(other.as_str())
-    }
-}
+pub struct LanguageTag(oxilangtag::LanguageTag<String>);
 
 impl Default for LanguageTag {
     fn default() -> Self {
@@ -28,7 +27,7 @@ impl Default for LanguageTag {
 }
 
 impl RequiredPrompt for LanguageTag {
-    const MESSAGE: &'static str = "Package locale";
+    const MESSAGE: &'static str = "Package locale:";
     const HELP_MESSAGE: Option<&'static str> = Some("Example: en-US");
     const PLACEHOLDER: Option<&'static str> = None;
 }
