def1eb5587460e64d23ca11802f7603b61662ce9
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2024-01-29 10:14:36 +0000

    Split list inputs by spaces and commas
    Resolves #410
    Resolves #412

--- src/prompts/list_prompt.rs
+++ src/prompts/list_prompt.rs
@@ -1,6 +1,6 @@
 use inquire::validator::Validation;
 use inquire::Text;
-use std::collections::{BTreeSet, HashSet};
+use std::collections::BTreeSet;
 use std::fmt::{Debug, Display};
 use std::str::FromStr;
 
@@ -15,13 +15,14 @@ where
     T: FromStr + ListPrompt + Ord,
     <T as FromStr>::Err: Display + Debug + Sync + Send + 'static,
 {
+    const DELIMITERS: [char; 2] = [' ', ','];
     let items = Text::new(T::MESSAGE)
         .with_help_message(T::HELP_MESSAGE)
         .with_validator(|input: &str| {
             let items = input
-                .split(|char: char| !char.is_alphanumeric())
+                .split(|char| DELIMITERS.contains(&char))
                 .filter(|str| !str.is_empty())
-                .collect::<HashSet<_>>();
+                .collect::<BTreeSet<_>>();
             let items_len = items.len();
             if items_len > T::MAX_ITEMS as usize {
                 return Ok(Validation::Invalid(
@@ -33,14 +34,14 @@ where
                 ));
             }
             for item in items {
-                if let Err(error) = T::from_str(input) {
+                if let Err(error) = T::from_str(item) {
                     return Ok(Validation::Invalid(format!("{item}: {error}").into()));
                 }
             }
             Ok(Validation::Valid)
         })
         .prompt()?
-        .split(|char: char| !char.is_alphanumeric())
+        .split(|char| DELIMITERS.contains(&char))
         .filter_map(|str| T::from_str(str).ok())
         .collect::<BTreeSet<_>>();
     if items.is_empty() {
