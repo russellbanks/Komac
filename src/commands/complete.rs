1d1309ce3abe8d2f21695d9f65d722b61f1193fe
Tom Plant <tom@tplant.com.au>
2025-05-11 23:17:22 +1000

    Add usage examples to `komac complete --help`
    Fixes #1219
    
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- src/commands/complete.rs
+++ src/commands/complete.rs
@@ -4,9 +4,15 @@ use clap_complete::{Shell, generate};
 
 use crate::Cli;
 
-/// Outputs an autocompletion script for the given shell
+/// Outputs an autocompletion script for the given shell. Example usage:
+/// 
+/// Bash: echo "source <(komac complete bash)" >> ~/.bashrc
+/// Elvish: echo "eval (komac complete elvish | slurp)" >> ~/.elvish/rc.elv
+/// Fish: echo "source (komac complete fish | psub)" >> ~/.config/fish/config.fish
+/// Powershell: echo "komac complete powershell | Out-String | Invoke-Expression" >> $PROFILE
+/// Zsh: echo "source <(komac complete zsh)" >> ~/.zshrc
 #[derive(Parser)]
-#[clap(visible_alias = "autocomplete")]
+#[clap(visible_alias = "autocomplete", verbatim_doc_comment)]
 pub struct Complete {
     #[arg(value_enum)]
     shell: Shell,
