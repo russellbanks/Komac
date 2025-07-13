4fc50759d6bf3444292ebecc69ae843bc805e00d
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2025-07-14 00:20:21 +0100

    Use `inno` crate

--- src/installers/inno/mod.rs
+++ src/installers/inno/mod.rs
@@ -1,247 +1,80 @@
-mod compression;
-mod encoding;
-mod entry;
-mod enum_value;
-mod flag_reader;
-mod header;
-mod loader;
-pub mod read;
-mod version;
-mod windows_version;
-mod wizard;
-
-use std::{io, io::Cursor, mem};
+use std::io::Cursor;
 
 use camino::Utf8PathBuf;
 use compact_str::CompactString;
 use const_format::formatcp;
-use encoding_rs::{UTF_16LE, WINDOWS_1252};
-use entry::language::Language;
-use itertools::Itertools;
+use inno::{
+    error::InnoError,
+    header::{Architecture as InnoArchitecture, PrivilegesRequiredOverrides},
+};
 use msi::Language as CodePageLanguage;
-use thiserror::Error;
-use tracing::{debug, trace};
 use winget_types::{
     LanguageTag, Sha256String, Version,
     installer::{
-        AppsAndFeaturesEntry, InstallationMetadata, Installer, InstallerType, Scope,
+        AppsAndFeaturesEntry, Architecture as WingetArchitecture, ElevationRequirement,
+        InstallationMetadata, Installer, InstallerType, Scope, UnsupportedOSArchitecture,
         switches::{CustomSwitch, InstallerSwitches},
     },
     url::DecodedUrl,
 };
-use yara_x::mods::{PE, pe::ResourceType};
-use zerocopy::TryFromBytes;
 
-use crate::installers::{
-    inno::{
-        entry::{
-            component::Component, directory::Directory, file::File, icon::Icon, ini::Ini,
-            message::Message, permission::Permission, registry::Registry, task::Task, r#type::Type,
-        },
-        header::{Header, flags::PrivilegesRequiredOverrides},
-        loader::{SETUP_LOADER_OFFSET, SETUP_LOADER_RESOURCE, SetupLoader, SetupLoaderOffset},
-        read::block::InnoBlockReader,
-        version::InnoVersion,
-        wizard::Wizard,
-    },
-    utils::{
-        RELATIVE_APP_DATA, RELATIVE_COMMON_FILES_32, RELATIVE_COMMON_FILES_64,
-        RELATIVE_LOCAL_APP_DATA, RELATIVE_PROGRAM_DATA, RELATIVE_PROGRAM_FILES_32,
-        RELATIVE_PROGRAM_FILES_64, RELATIVE_SYSTEM_DRIVE, RELATIVE_SYSTEM_ROOT,
-        RELATIVE_WINDOWS_DIR,
-    },
+use super::utils::{
+    RELATIVE_APP_DATA, RELATIVE_COMMON_FILES_32, RELATIVE_COMMON_FILES_64, RELATIVE_LOCAL_APP_DATA,
+    RELATIVE_PROGRAM_DATA, RELATIVE_PROGRAM_FILES_32, RELATIVE_PROGRAM_FILES_64,
+    RELATIVE_SYSTEM_DRIVE, RELATIVE_SYSTEM_ROOT, RELATIVE_WINDOWS_DIR,
 };
-
-const VERSION_LEN: usize = 1 << 6;
-
-const MAX_SUPPORTED_VERSION: InnoVersion = InnoVersion::new(6, 4, u8::MAX, 0);
-
-#[derive(Error, Debug)]
-pub enum InnoError {
-    #[error("File is not an Inno installer")]
-    NotInnoFile,
-    #[error("Invalid Inno header version")]
-    InvalidSetupHeader,
-    #[error(
-        "Inno Setup version {0} is newer than the maximum supported version {MAX_SUPPORTED_VERSION}"
-    )]
-    UnsupportedVersion(InnoVersion),
-    #[error("Unknown Inno setup version: {0}")]
-    UnknownVersion(String),
-    #[error("Unknown Inno Setup loader signature: {0:?}")]
-    UnknownLoaderSignature([u8; 12]),
-    #[error("Inno CRC32 checksum mismatch. Expected {expected} but calculated {actual}")]
-    CrcChecksumMismatch { actual: u32, expected: u32 },
-    #[error(transparent)]
-    Io(#[from] io::Error),
-}
 
 pub struct Inno {
     pub installers: Vec<Installer>,
 }
 
 impl Inno {
-    pub fn new(data: &[u8], pe: &PE) -> Result<Self, InnoError> {
-        // Before Inno 5.1.5, the offset table is found by following a pointer at a constant offset
-        let setup_loader = data
-            .get(SETUP_LOADER_OFFSET..SETUP_LOADER_OFFSET + size_of::<SetupLoaderOffset>())
-            .and_then(|data| SetupLoaderOffset::try_ref_from_bytes(data).ok())
-            .filter(|offset| offset.table_offset == !offset.not_table_offset)
-            .and_then(|offset| data.get(offset.table_offset.get() as usize..))
-            .map(SetupLoader::new)
-            .or_else(|| {
-                // From Inno 5.1.5, the offset table is stored as a PE resource entry
-                pe.resources
-                    .iter()
-                    .filter(|resource| resource.type_() == ResourceType::RESOURCE_TYPE_RCDATA)
-                    .find(|resource| resource.id() == SETUP_LOADER_RESOURCE)
-                    .and_then(|resource| {
-                        let offset = resource.offset() as usize;
-                        data.get(offset..offset + resource.length() as usize)
-                    })
-                    .map(SetupLoader::new)
-            })
-            .ok_or(InnoError::NotInnoFile)??;
+    pub fn new(data: &[u8]) -> Result<Self, InnoError> {
+        let mut inno = inno::Inno::new(Cursor::new(data))?;
 
-        let header_offset = setup_loader.header_offset as usize;
-        let version_bytes = data
-            .get(header_offset..header_offset + VERSION_LEN)
-            .and_then(|bytes| memchr::memchr(0, bytes).map(|len| &bytes[..len]))
-            .ok_or(InnoError::InvalidSetupHeader)?;
-
-        debug!(raw_version = ?std::str::from_utf8(version_bytes));
-
-        let inno_version = InnoVersion::from_version_bytes(version_bytes).ok_or_else(|| {
-            InnoError::UnknownVersion(String::from_utf8_lossy(version_bytes).into_owned())
-        })?;
-
-        debug!(?inno_version);
-
-        if inno_version > MAX_SUPPORTED_VERSION {
-            return Err(InnoError::UnsupportedVersion(inno_version));
-        }
-
-        let mut cursor = Cursor::new(data);
-        cursor.set_position((header_offset + VERSION_LEN) as u64);
-
-        let mut reader = InnoBlockReader::get(cursor, &inno_version)?;
-
-        let mut codepage = if inno_version.is_unicode() {
-            UTF_16LE
-        } else {
-            WINDOWS_1252
-        };
-
-        let mut header = Header::from_reader(&mut reader, codepage, &inno_version)?;
-
-        debug!(?header);
-
-        let languages = (0..header.language_count)
-            .map(|_| Language::from_reader(&mut reader, codepage, &inno_version))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        if !inno_version.is_unicode() {
-            codepage = languages
-                .iter()
-                .map(|language| language.codepage)
-                .find_or_first(|&codepage| codepage == WINDOWS_1252)
-                .unwrap_or(WINDOWS_1252);
-        }
-
-        if inno_version < (4, 0, 0) {
-            debug!("Reading images and plugins");
-            Wizard::from_reader(&mut reader, &inno_version, &header)?;
-        }
-
-        trace!("Reading messages");
-        let _messages = (0..header.message_count)
-            .map(|_| Message::from_reader(&mut reader, &languages, codepage))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        trace!("Reading permissions");
-        let _permissions = (0..header.permission_count)
-            .map(|_| Permission::from_reader(&mut reader, codepage))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        trace!("Reading type entries");
-        let _type_entries = (0..header.type_count)
-            .map(|_| Type::from_reader(&mut reader, codepage, &inno_version))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        trace!("Reading components");
-        let _components = (0..header.component_count)
-            .map(|_| Component::from_reader(&mut reader, codepage, &inno_version))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        trace!("Reading tasks");
-        let _tasks = (0..header.task_count)
-            .map(|_| Task::from_reader(&mut reader, codepage, &inno_version))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        trace!("Reading directories");
-        let _directories = (0..header.directory_count)
-            .map(|_| Directory::from_reader(&mut reader, codepage, &inno_version))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        trace!("Reading files");
-        let _files = (0..header.file_count)
-            .map(|_| File::from_reader(&mut reader, codepage, &inno_version))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        trace!("Reading icons");
-        let _icons = (0..header.icon_count)
-            .map(|_| Icon::from_reader(&mut reader, codepage, &inno_version))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        trace!("Reading ini entries");
-        let _ini = (0..header.ini_entry_count)
-            .map(|_| Ini::from_reader(&mut reader, codepage, &inno_version))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        trace!("Reading registry entries");
-        let _registry = (0..header.registry_entry_count)
-            .map(|_| Registry::from_reader(&mut reader, codepage, &inno_version))
-            .collect::<io::Result<Vec<_>>>()?;
-
-        let install_dir = header
+        let install_dir = inno
+            .header
             .default_dir_name
             .take()
             .map(to_relative_install_dir)
             .filter(|dir| !dir.contains(['{', '}']));
 
         let mut installer = Installer {
-            locale: languages.first().and_then(|language_entry| {
+            locale: inno.languages.first().and_then(|language_entry| {
                 CodePageLanguage::from_code(u16::try_from(language_entry.id).ok()?)
                     .tag()
                     .parse::<LanguageTag>()
                     .ok()
             }),
-            architecture: mem::take(&mut header.architectures_allowed).into(),
+            architecture: WingetArchitecture::from_inno(inno.header.architectures_allowed),
             r#type: Some(InstallerType::Inno),
             scope: install_dir
                 .as_deref()
                 .and_then(Scope::from_install_directory),
             url: DecodedUrl::default(),
             sha_256: Sha256String::default(),
-            product_code: header.app_id.clone().map(to_product_code),
-            unsupported_os_architectures: header.architectures_disallowed.into(),
-            apps_and_features_entries: if header.uninstall_name.is_some()
-                || header.app_publisher.is_some()
-                || header.app_version.is_some()
+            product_code: inno.header.app_id.clone().map(to_product_code),
+            unsupported_os_architectures: UnsupportedOSArchitecture::from_inno(
+                inno.header.architectures_disallowed,
+            ),
+            apps_and_features_entries: if inno.header.uninstall_name.is_some()
+                || inno.header.app_publisher.is_some()
+                || inno.header.app_version.is_some()
             {
                 vec![AppsAndFeaturesEntry {
-                    display_name: header.uninstall_name.take().map(CompactString::from),
-                    publisher: header.app_publisher.take().map(CompactString::from),
-                    display_version: header.app_version.as_deref().map(Version::new),
-                    product_code: header.app_id.take().map(to_product_code),
+                    display_name: inno.header.uninstall_name.take().map(CompactString::from),
+                    publisher: inno.header.app_publisher.take().map(CompactString::from),
+                    display_version: inno.header.app_version.as_deref().map(Version::new),
+                    product_code: inno.header.product_code(),
                     ..AppsAndFeaturesEntry::default()
                 }]
             } else {
                 vec![]
             },
-            elevation_requirement: header
+            elevation_requirement: inno
+                .header
                 .privileges_required
-                .to_elevation_requirement(&header.privileges_required_overrides_allowed),
+                .to_elevation_requirement(&inno.header.privileges_required_overrides_allowed),
             installation_metadata: InstallationMetadata {
                 default_install_location: install_dir.map(Utf8PathBuf::from),
                 ..InstallationMetadata::default()
@@ -249,11 +82,12 @@ impl Inno {
             ..Default::default()
         };
 
-        let installers = if header.privileges_required_overrides_allowed.is_empty() {
+        let installers = if inno.header.privileges_required_overrides_allowed.is_empty() {
             vec![installer]
         } else {
             installer.scope = Some(Scope::Machine);
-            let has_scope_switch = header
+            let has_scope_switch = inno
+                .header
                 .privileges_required_overrides_allowed
                 .contains(PrivilegesRequiredOverrides::COMMAND_LINE);
             if has_scope_switch {
@@ -275,6 +109,63 @@ impl Inno {
         };
 
         Ok(Self { installers })
+    }
+}
+
+trait PrivilegeLevelExt {
+    fn to_elevation_requirement(
+        &self,
+        overrides: &PrivilegesRequiredOverrides,
+    ) -> Option<ElevationRequirement>;
+}
+
+impl PrivilegeLevelExt for inno::header::PrivilegeLevel {
+    fn to_elevation_requirement(
+        &self,
+        overrides: &PrivilegesRequiredOverrides,
+    ) -> Option<ElevationRequirement> {
+        match self {
+            Self::Admin | Self::PowerUser => Some(ElevationRequirement::ElevatesSelf),
+            _ if !overrides.is_empty() => Some(ElevationRequirement::ElevatesSelf),
+            _ => None,
+        }
+    }
+}
+
+pub trait FromInnoArch {
+    fn from_inno(value: InnoArchitecture) -> Self;
+}
+
+impl FromInnoArch for WingetArchitecture {
+    fn from_inno(value: InnoArchitecture) -> Self {
+        if value.intersects(
+            InnoArchitecture::X64_OS | InnoArchitecture::WIN64 | InnoArchitecture::X64_COMPATIBLE,
+        ) {
+            Self::X64
+        } else if value.intersects(InnoArchitecture::ARM64 | InnoArchitecture::ARM32_COMPATIBLE) {
+            Self::Arm64
+        } else {
+            Self::X86
+        }
+    }
+}
+
+impl FromInnoArch for UnsupportedOSArchitecture {
+    fn from_inno(value: InnoArchitecture) -> Self {
+        value.iter().fold(
+            Self::empty(),
+            |unsupported_arch, architecture| match architecture {
+                InnoArchitecture::X64_OS
+                | InnoArchitecture::WIN64
+                | InnoArchitecture::X64_COMPATIBLE => unsupported_arch | Self::X64,
+                InnoArchitecture::ARM64 => unsupported_arch | Self::ARM64,
+                InnoArchitecture::ARM32_COMPATIBLE => unsupported_arch | Self::ARM,
+                InnoArchitecture::X86_OS | InnoArchitecture::X86_COMPATIBLE => {
+                    unsupported_arch | Self::X86
+                }
+                _ => unsupported_arch,
+            },
+        )
     }
 }
 
