0ad870230c6cabadf550d5351d11b1fa343260e3
Tom Plant <tom.plant@devicie.com>
2026-03-31 22:45:28 +0000

    Add 64-bit roots to NSIS registry Skipped 32-bit (0x40000000 context) because I don't have a test case
    Signed-off-by: Tom Plant <tom.plant@devicie.com>

--- src/analysis/installers/utils/registry.rs
+++ src/analysis/installers/utils/registry.rs
@@ -19,6 +19,16 @@ pub enum RegRoot {
     HKeyDynamicData = 0x8000_0006u32.to_le(),
     HKeyPerformanceText = 0x8000_0050u32.to_le(),
     HKeyPerformanceNLSText = 0x8000_0060u32.to_le(),
+    ShellContext64 = 0x2000_0000u32.to_le(),
+    HKeyClassesRoot64 = 0xA000_0000u32.to_le(),
+    HKeyCurrentUser64 = 0xA000_0001u32.to_le(),
+    HKeyLocalMachine64 = 0xA000_0002u32.to_le(),
+    HKeyUsers64 = 0xA000_0003u32.to_le(),
+    HKeyPerformanceData64 = 0xA000_0004u32.to_le(),
+    HKeyCurrentConfig64 = 0xA000_0005u32.to_le(),
+    HKeyDynamicData64 = 0xA000_0006u32.to_le(),
+    HKeyPerformanceText64 = 0xA000_0050u32.to_le(),
+    HKeyPerformanceNLSText64 = 0xA000_0060u32.to_le(),
 }
 
 impl fmt::Display for RegRoot {
@@ -34,6 +44,16 @@ impl fmt::Display for RegRoot {
             Self::HKeyDynamicData => f.write_str("HKEY_DYNAMIC_DATA"),
             Self::HKeyPerformanceText => f.write_str("HKEY_PERFORMANCE"),
             Self::HKeyPerformanceNLSText => f.write_str("HKEY_PERFORMANCE_NLSTEXT"),
+            Self::ShellContext64 => f.write_str("SHELL_CONTEXT (64-bit)"),
+            Self::HKeyClassesRoot64 => f.write_str("HKEY_CLASSES_ROOT (64-bit)"),
+            Self::HKeyCurrentUser64 => f.write_str("HKEY_CURRENT_USER (64-bit)"),
+            Self::HKeyLocalMachine64 => f.write_str("HKEY_LOCAL_MACHINE (64-bit)"),
+            Self::HKeyUsers64 => f.write_str("HKEY_USERS (64-bit)"),
+            Self::HKeyPerformanceData64 => f.write_str("HKEY_PERFORMANCE_DATA (64-bit)"),
+            Self::HKeyCurrentConfig64 => f.write_str("HKEY_CURRENT_CONFIG (64-bit)"),
+            Self::HKeyDynamicData64 => f.write_str("HKEY_DYNAMIC_DATA (64-bit)"),
+            Self::HKeyPerformanceText64 => f.write_str("HKEY_PERFORMANCE (64-bit)"),
+            Self::HKeyPerformanceNLSText64 => f.write_str("HKEY_PERFORMANCE_NLSTEXT (64-bit)"),
         }
     }
 }
