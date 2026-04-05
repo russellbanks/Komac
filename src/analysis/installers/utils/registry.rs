0d2711c83eaa256d3abd62a5e4016ab99860bade
Tom Plant <tom@tplant.com.au>
2026-04-05 20:09:21 +1000

    Add 64-bit roots to NSIS registry (#1691)
    * Add 64-bit roots to NSIS registry
    * Use _64 suffix instead of (64-bit)
    
    ---------
    
    Signed-off-by: Tom Plant <tom.plant@devicie.com>
    Co-authored-by: Russell Banks <74878137+russellbanks@users.noreply.github.com>

--- src/analysis/installers/utils/registry.rs
+++ src/analysis/installers/utils/registry.rs
@@ -19,6 +19,16 @@ pub enum RegRoot {
     HKeyDynamicData = 0x8000_0006u32.to_le(),
     HKeyPerformanceText = 0x8000_0050u32.to_le(),
     HKeyPerformanceNLSText = 0x8000_0060u32.to_le(),
+    ShellContext64 = 0x2000_0000u32.to_le(),
+    HKeyClassesRoot64 = 0xA000_0000u32.to_le(),
+    HKeyCurrentUser64 = 0xA000_0001u32.to_le(),
+    HKeyLocalMachine64 = 0xA000_0002u32.to_le(),
+    HKeyUsers64 = 0xA000_0003u32.to_le(),
+    HKeyPerformanceData64 = 0xA000_0004u32.to_le(),
+    HKeyCurrentConfig64 = 0xA000_0005u32.to_le(),
+    HKeyDynamicData64 = 0xA000_0006u32.to_le(),
+    HKeyPerformanceText64 = 0xA000_0050u32.to_le(),
+    HKeyPerformanceNLSText64 = 0xA000_0060u32.to_le(),
 }
 
 impl fmt::Display for RegRoot {
@@ -34,6 +44,16 @@ impl fmt::Display for RegRoot {
             Self::HKeyDynamicData => f.write_str("HKEY_DYNAMIC_DATA"),
             Self::HKeyPerformanceText => f.write_str("HKEY_PERFORMANCE"),
             Self::HKeyPerformanceNLSText => f.write_str("HKEY_PERFORMANCE_NLSTEXT"),
+            Self::ShellContext64 => f.write_str("SHELL_CONTEXT_64"),
+            Self::HKeyClassesRoot64 => f.write_str("HKEY_CLASSES_ROOT_64"),
+            Self::HKeyCurrentUser64 => f.write_str("HKEY_CURRENT_USER_64"),
+            Self::HKeyLocalMachine64 => f.write_str("HKEY_LOCAL_MACHINE_64"),
+            Self::HKeyUsers64 => f.write_str("HKEY_USERS_64"),
+            Self::HKeyPerformanceData64 => f.write_str("HKEY_PERFORMANCE_DATA_64"),
+            Self::HKeyCurrentConfig64 => f.write_str("HKEY_CURRENT_CONFIG_64"),
+            Self::HKeyDynamicData64 => f.write_str("HKEY_DYNAMIC_DATA_64"),
+            Self::HKeyPerformanceText64 => f.write_str("HKEY_PERFORMANCE_64"),
+            Self::HKeyPerformanceNLSText64 => f.write_str("HKEY_PERFORMANCE_NLSTEXT_64"),
         }
     }
 }
