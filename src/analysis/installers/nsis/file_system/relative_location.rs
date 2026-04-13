bac131864578b713833e37a2305cf481b6735455
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2026-04-13 21:50:19 +0100

    Add `is_` methods to NSIS RelativeLocation

--- src/analysis/installers/nsis/file_system/relative_location.rs
+++ src/analysis/installers/nsis/file_system/relative_location.rs
@@ -6,6 +6,26 @@ pub enum RelativeLocation {
     Current,
 }
 
+impl RelativeLocation {
+    /// Returns `true` if the relative location is [`Root`].
+    ///
+    /// [`Root`]: Self::Root
+    #[must_use]
+    #[inline]
+    pub const fn is_root(self) -> bool {
+        matches!(self, Self::Root)
+    }
+
+    /// Returns `true` if the relative location is [`Current`].
+    ///
+    /// [`Current`]: Self::Current
+    #[must_use]
+    #[inline]
+    pub const fn is_current(self) -> bool {
+        matches!(self, Self::Current)
+    }
+}
+
 impl fmt::Display for RelativeLocation {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fmt::Debug::fmt(self, f)
