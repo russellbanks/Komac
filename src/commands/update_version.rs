russellbanks/Komac

commit 44866a8326f32e8d026c56eb7d6a0dec40aacafd

Support updating to different number of installers
Resolves #210

src/commands/update_version.rs
@@ -1,44 +1,33 @@
 use std::{
     collections::BTreeSet,
-    io::{Read, Seek},
-    mem,
     num::{NonZeroU32, NonZeroUsize},
+    path::PathBuf,
 };
 
 use anstream::println;
-use camino::Utf8PathBuf;
 use clap::Parser;
 use color_eyre::eyre::{Error, Result, bail};
 use futures_util::TryFutureExt;
 use indicatif::ProgressBar;
-use itertools::Itertools;
 use owo_colors::OwoColorize;
 use secrecy::SecretString;
-use strsim::levenshtein;
 use tokio::try_join;
 use winget_types::{
     PackageIdentifier, PackageVersion,
-    installer::{InstallerType, MinimumOSVersion, NestedInstallerFiles},
     url::{DecodedUrl, ReleaseNotesUrl},
 };
 
 use crate::{
-    analysis::installers::Zip,
-    commands::utils::{
-        SPINNER_TICK_RATE, SubmitOption, prompt_existing_pull_request, write_changes_to_dir,
-    },
+    analysis::Analyzer,
+    commands::utils::{SPINNER_TICK_RATE, SubmitOption},
     download::Downloader,
-    download_file::process_files,
     github::{
         GITHUB_HOST, GitHubError, WINGET_PKGS_FULL_NAME,
         client::{GitHub, GitHubValues},
-        graphql::get_existing_pull_request::PullRequest,
-        utils::{PackagePath, pull_request::pr_changes},
+        utils::{PackagePath, pull_request::Change},
     },
-    manifests::Url,
-    match_installers::match_installers,
+    manifests::{Url, print_changes},
     token::TokenManager,
-    traits::{LocaleExt, path::NormalizePath},
 };
 
 /// Add a version to a pre-existing package
@@ -46,12 +35,12 @@ use crate::{
 #[derive(Parser)]
 pub struct UpdateVersion {
     /// The package's unique identifier
-    #[arg()]
-    package_identifier: PackageIdentifier,
+    #[arg(value_name = "PACKAGE_IDENTIFIER")]
+    identifier: PackageIdentifier,
 
     /// The package's version
     #[arg(short = 'v', long = "version")]
-    package_version: PackageVersion,
+    version: PackageVersion,
 
     /// The list of package installers
     #[arg(short, long, num_args = 1.., required = true, value_hint = clap::ValueHint::Url)]
@@ -83,7 +72,7 @@ pub struct UpdateVersion {
 
     /// Directory to output the manifests to
     #[arg(short, long, env = "OUTPUT_DIRECTORY", value_hint = clap::ValueHint::DirPath)]
-    output: Option<Utf8PathBuf>,
+    output: Option<PathBuf>,
 
     /// Open pull request link automatically
     #[arg(long, env = "OPEN_PR")]
@@ -111,156 +100,72 @@ impl UpdateVersion {
         let token_manager = TokenManager::handle(self.token.take()).await?;
         let github = GitHub::new(&token_manager)?;
 
-        let (versions, existing_pr) = try_join!(
-            github.get_versions(&self.package_identifier),
-            github.get_existing_pull_request(&self.package_identifier, &self.package_version),
-        )?;
+        let mut package = github
+            .get_versioned_package(&self.identifier, &self.version)
+            .await?;
 
-        let latest_version = versions.last().unwrap_or_else(|| unreachable!());
         println!(
-            "Latest version of {}: {latest_version}",
-            self.package_identifier
+            "Latest version of {}: {}",
+            self.identifier,
+            package.latest_version()
         );
 
-        let replace_version = self.resolve_replace_version(&versions, latest_version)?;
-
-        if self.should_abort_for_existing_pr(existing_pr)? {
+        if self.skip_pr_check || self.dry_run || !package.prompt_existing_pr()? {
             return Ok(());
         }
 
+        let replace_version = self
+            .resolve_replace_version(package.versions(), package.latest_version())?
+            .cloned();
+
         let downloader = Downloader::new_with_concurrent(self.concurrent_downloads)?;
-        let (mut manifests, mut github_values, mut files) = try_join!(
-            github
-                .get_manifests(&self.package_identifier, latest_version)
-                .map_err(Error::new),
+        let (mut github_values, mut files) = try_join!(
             self.fetch_github_values(&github).map_err(Error::new),
             downloader.download(self.urls.iter().cloned()),
         )?;
 
-        let mut download_results = process_files(&mut files).await?;
-        let installer_results = download_results
-            .iter_mut()
-            .flat_map(|(_url, analyzer)| mem::take(&mut analyzer.installers))
-            .collect::<Vec<_>>();
-        let previous_installers = mem::take(&mut manifests.installer.installers)
-            .into_iter()
-            .map(|mut installer| {
-                if manifests.installer.r#type.is_some() {
-                    installer.r#type = manifests.installer.r#type;
-                }
-                if manifests.installer.nested_installer_type.is_some() {
-                    installer.nested_installer_type = manifests.installer.nested_installer_type;
-                }
-                if manifests.installer.scope.is_some() {
-                    installer.scope = manifests.installer.scope;
-                }
-                installer
-            })
-            .collect::<Vec<_>>();
+        let manifests = package.manifests_mut().unwrap();
 
-        let duplicate_urls = previous_installers
-            .iter()
-            .map(|installer| installer.url.clone())
-            .duplicates()
-            .collect::<Vec<_>>();
+        let download_results = files.analyze().await?;
 
-        manifests.default_locale.package_version = self.package_version.clone();
-        let matched_installers = match_installers(previous_installers, &installer_results);
-        let installers = matched_installers
-            .into_iter()
-            .map(|(previous_installer, new_installer)| {
-                let analyzer = &download_results[&new_installer.url];
-                let installer_type = match previous_installer.r#type {
-                    Some(InstallerType::Portable) => previous_installer.r#type,
-                    _ => match new_installer.r#type {
-                        Some(InstallerType::Portable) => previous_installer.r#type,
-                        _ => new_installer.r#type,
-                    },
-                };
-
-                let previous_nested_files = previous_installer.nested_installer_files.clone();
-                let previous_url = previous_installer.url.clone();
-                let previous_architecture = previous_installer.architecture;
-
-                let mut installer = new_installer.clone().merge_with(previous_installer);
-                installer.r#type = installer_type;
-                installer.url.clone_from(&new_installer.url);
-
-                let nested_files_to_fix = [
-                    &previous_nested_files,
-                    &manifests.installer.nested_installer_files,
-                    &installer.nested_installer_files,
-                ]
-                .into_iter()
-                .find(|files| !files.is_empty())
-                .cloned();
-
-                if let Some(nested_files) = nested_files_to_fix {
-                    installer.nested_installer_files =
-                        fix_relative_paths(nested_files, analyzer.zip.as_ref());
-                }
-
-                if duplicate_urls.contains(&previous_url) {
-                    installer.architecture = previous_architecture;
-                }
-
-                for entry in &mut installer.apps_and_features_entries {
-                    entry.deduplicate(&manifests.default_locale);
-                }
-                installer
-            })
-            .collect::<Vec<_>>();
-
-        manifests.installer.package_version = self.package_version.clone();
-        manifests.installer.minimum_os_version = manifests
-            .installer
-            .minimum_os_version
-            .filter(|minimum_os_version| *minimum_os_version != MinimumOSVersion::new(10, 0, 0, 0));
-        manifests.installer.installers = installers;
+        manifests.installer.package_version = self.version.clone();
+        manifests.installer.installers = download_results
+            .into_values()
+            .flat_map(Analyzer::into_installers)
+            .collect();
         manifests.installer.optimize();
 
-        manifests.default_locale.update(
-            &self.package_version,
+        manifests.update(
+            &self.version,
             &mut github_values,
             self.release_notes_url.as_ref(),
         );
 
-        manifests.locales.iter_mut().for_each(|locale| {
-            locale.update(
-                &self.package_version,
-                &mut github_values,
-                self.release_notes_url.as_ref(),
-            );
-        });
+        let mut changes = manifests.create(
+            &self.identifier,
+            &self.version,
+            self.created_with.as_deref(),
+        );
 
-        manifests.version.update(&self.package_version);
+        if self.dry_run {
+            print_changes(changes.iter().map(Change::manifest));
+            return Ok(());
+        }
 
-        let package_path =
-            PackagePath::new(&self.package_identifier, Some(&self.package_version), None);
-        let mut changes = pr_changes()
-            .package_identifier(&self.package_identifier)
-            .manifests(&manifests)
-            .package_path(&package_path)
-            .maybe_created_with(self.created_with.as_deref())
-            .create()?;
+        let submit_option =
+            SubmitOption::prompt(&mut changes, &self.identifier, &self.version, self.submit)?;
 
-        let submit_option = SubmitOption::prompt(
-            &mut changes,
-            &self.package_identifier,
-            &self.package_version,
-            self.submit,
-            self.dry_run,
-        )?;
-
+        let package_path = PackagePath::new(&self.identifier, Some(&self.version), None);
         if let Some(output) = self
             .output
             .as_ref()
             .map(|out| out.join(package_path.as_str()))
         {
-            write_changes_to_dir(&changes, output.as_path()).await?;
+            changes.write_to(output.as_path()).await?;
             println!(
-                "{} written all manifest files to {output}",
-                "Successfully".green()
+                "{} written all manifest files to {}",
+                "Successfully".green(),
+                output.display()
             );
         }
 
@@ -271,17 +176,17 @@ impl UpdateVersion {
         // Create an indeterminate progress bar to show as a pull request is being created
         let pr_progress = ProgressBar::new_spinner().with_message(format!(
             "Creating a pull request for {} {}",
-            self.package_identifier, self.package_version
+            self.identifier, self.version
         ));
         pr_progress.enable_steady_tick(SPINNER_TICK_RATE);
 
         let pull_request = github
             .add_version()
-            .identifier(&self.package_identifier)
-            .version(&self.package_version)
-            .versions(&versions)
+            .identifier(&self.identifier)
+            .version(&self.version)
+            .versions(package.versions())
             .changes(changes)
-            .maybe_replace_version(replace_version)
+            .maybe_replace_version(replace_version.as_ref())
             .issue_resolves(&self.resolves)
             .maybe_created_with(self.created_with.as_deref())
             .maybe_created_with_url(self.created_with_url.as_ref())
@@ -314,7 +219,7 @@ impl UpdateVersion {
                     version
                 }
             })
-            .filter(|&version| version.as_str() != self.package_version.as_str());
+            .filter(|&version| version.as_str() != self.version.as_str());
 
         if let Some(version) = replace_version
             && !versions.contains(version)
@@ -326,25 +231,6 @@ impl UpdateVersion {
         }
 
         Ok(replace_version)
-    }
-
-    fn should_abort_for_existing_pr<T>(&self, existing_pr: T) -> Result<bool>
-    where
-        T: Into<Option<PullRequest>>,
-    {
-        if let Some(ref pull_request) = existing_pr.into()
-            && !self.skip_pr_check
-            && !self.dry_run
-            && !prompt_existing_pull_request(
-                &self.package_identifier,
-                &self.package_version,
-                pull_request,
-            )?
-        {
-            return Ok(true);
-        }
-
-        Ok(false)
     }
 
     async fn fetch_github_values(
@@ -364,38 +250,4 @@ impl UpdateVersion {
             Ok(None)
         }
     }
-}
-
-fn fix_relative_paths<R: Read + Seek>(
-    nested_installer_files: BTreeSet<NestedInstallerFiles>,
-    zip: Option<&Zip<R>>,
-) -> BTreeSet<NestedInstallerFiles> {
-    let Some(zip) = zip else {
-        return nested_installer_files;
-    };
-
-    nested_installer_files
-        .into_iter()
-        .filter_map(|nested_installer_files| {
-            if zip
-                .possible_installer_files
-                .contains(&nested_installer_files.relative_file_path.normalize())
-            {
-                Some(nested_installer_files)
-            } else {
-                zip.possible_installer_files
-                    .iter()
-                    .min_by_key(|file_path| {
-                        levenshtein(
-                            file_path.as_str(),
-                            nested_installer_files.relative_file_path.as_str(),
-                        )
-                    })
-                    .map(|path| NestedInstallerFiles {
-                        relative_file_path: path.to_path_buf(),
-                        ..nested_installer_files
-                    })
-            }
-        })
-        .collect::<BTreeSet<_>>()
 }

Cargo.toml
@@ -101,7 +101,7 @@ typed-path = "0.12.3"
 url = { version = "2.5.8", features = ["serde"] }
 uuid = { version = "1.23.3", features = ["serde", "v4"] }
 walkdir = "2.5.0"
-winget-types = { version = "0.4.3", features = ["serde", "std", "chrono"] }
+winget-types = { version = "0.5.0", features = ["serde", "std", "chrono"] }
 zerocopy = { version = "0.8.52", features = ["derive", "std"] }
 zip = { version = "8.6.0", default-features = false, features = ["deflate"] }
 

src/analysis/analyzer.rs
@@ -74,6 +74,11 @@ impl<'reader, R: Read + Seek> Analyzer<'reader, R> {
             ..Self::default()
         })
     }
+
+    /// Consumes the [`Analyzer`], returning the inner installers.
+    pub fn into_installers(self) -> Vec<Installer> {
+        self.installers
+    }
 }
 
 impl<R: Read + Seek> Default for Analyzer<'_, R> {

src/analysis/installers/advanced/footer.rs
@@ -45,7 +45,7 @@ pub struct Footer {
 impl Footer {
     pub const SIGNATURE: &[u8; 10] = b"ADVINSTSFX";
 
-    pub const SIGNATURE_OFFSET: usize = offset_of!(Self, signature);
+    const SIGNATURE_OFFSET: usize = offset_of!(Self, signature);
 
     pub fn find<R: Read + Seek>(reader: &mut R) -> Result<Self, AdvancedInstallerError> {
         const SEARCH_BLOCK_SIZE: usize = 16 * 1024;
@@ -121,8 +121,6 @@ impl fmt::Debug for Footer {
 
 #[cfg(test)]
 mod tests {
-    use std::mem::offset_of;
-
     use super::Footer;
 
     #[test]

src/analysis/installers/advanced/mod.rs
@@ -2,10 +2,7 @@ mod file_entry;
 mod footer;
 mod named_file_entry;
 
-use std::{
-    collections::BTreeSet,
-    io::{self, Cursor, Read, Seek, SeekFrom},
-};
+use std::io::{self, Cursor, Read, Seek, SeekFrom};
 
 use encoding_rs::UTF_16LE;
 use file_entry::FileEntry;
@@ -15,8 +12,8 @@ use sevenz_rust2::{ArchiveReader, Password};
 use thiserror::Error;
 use tracing::{debug, warn};
 use winget_types::installer::{
-    AppsAndFeaturesEntry, ExpectedReturnCodes, Installer, InstallerReturnCode, InstallerSwitches,
-    InstallerType, ReturnResponse,
+    AppsAndFeaturesEntry, ExpectedReturnCode, Installer, InstallerReturnCode, InstallerType,
+    ReturnResponse, Switches,
 };
 use zerocopy::IntoBytes;
 
@@ -113,7 +110,7 @@ impl Installers for AdvancedInstaller {
                 installer.r#type = Some(InstallerType::Exe);
 
                 // https://www.advancedinstaller.com/user-guide/exe-setup-file.html#proprietary-command-line-switches-for-the-exe-setup
-                installer.switches = InstallerSwitches::builder()
+                installer.switches = Switches::builder()
                     .silent("/exenoui /quiet".parse().unwrap())
                     .silent_with_progress("/exenoui /passive".parse().unwrap())
                     .install_location(r#"APPDIR="<INSTALLPATH>""#.parse().unwrap())
@@ -128,7 +125,7 @@ impl Installers for AdvancedInstaller {
                     .build();
 
                 // https://www.advancedinstaller.com/user-guide/exe-setup-file.html#return-code
-                installer.expected_return_codes = expected_return_codes();
+                installer.expected_return_codes = expected_return_codes().into();
 
                 // If the MSI is hidden, there's another ARP entry that shares some values
                 if msi
@@ -158,39 +155,80 @@ impl Installers for AdvancedInstaller {
     }
 }
 
-fn expected_return_codes() -> BTreeSet<ExpectedReturnCodes> {
+const fn expected_return_codes() -> [ExpectedReturnCode; 20] {
     use ReturnResponse::{
         AlreadyInstalled, BlockedByPolicy, CancelledByUser, ContactSupport, InstallInProgress,
         InvalidParameter, RebootInitiated, RebootRequiredToFinish, SystemNotSupported,
     };
 
     [
-        (-1, CancelledByUser),
-        (1, InvalidParameter),
-        (87, InvalidParameter),
-        (1601, ContactSupport),
-        (1602, CancelledByUser),
-        (1618, InstallInProgress),
-        (1623, SystemNotSupported),
-        (1625, BlockedByPolicy),
-        (1628, InvalidParameter),
-        (1633, SystemNotSupported),
-        (1638, AlreadyInstalled),
-        (1639, InvalidParameter),
-        (1640, BlockedByPolicy),
-        (1641, RebootInitiated),
-        (1643, BlockedByPolicy),
-        (1644, BlockedByPolicy),
-        (1649, BlockedByPolicy),
-        (1650, InvalidParameter),
-        (1654, SystemNotSupported),
-        (3010, RebootRequiredToFinish),
+        ExpectedReturnCode::new(InstallerReturnCode::from_i32(-1).unwrap(), CancelledByUser),
+        ExpectedReturnCode::new(InstallerReturnCode::from_u32(1).unwrap(), InvalidParameter),
+        ExpectedReturnCode::new(InstallerReturnCode::from_u32(87).unwrap(), InvalidParameter),
+        ExpectedReturnCode::new(InstallerReturnCode::from_u32(1601).unwrap(), ContactSupport),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1602).unwrap(),
+            CancelledByUser,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1618).unwrap(),
+            InstallInProgress,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1623).unwrap(),
+            SystemNotSupported,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1625).unwrap(),
+            BlockedByPolicy,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1628).unwrap(),
+            InvalidParameter,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1633).unwrap(),
+            SystemNotSupported,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1638).unwrap(),
+            AlreadyInstalled,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1639).unwrap(),
+            InvalidParameter,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1640).unwrap(),
+            BlockedByPolicy,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1641).unwrap(),
+            RebootInitiated,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1643).unwrap(),
+            BlockedByPolicy,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1644).unwrap(),
+            BlockedByPolicy,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1649).unwrap(),
+            BlockedByPolicy,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1650).unwrap(),
+            InvalidParameter,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(1654).unwrap(),
+            SystemNotSupported,
+        ),
+        ExpectedReturnCode::new(
+            InstallerReturnCode::from_u32(3010).unwrap(),
+            RebootRequiredToFinish,
+        ),
     ]
-    .into_iter()
-    .map(|(code, response)| ExpectedReturnCodes {
-        installer_return_code: InstallerReturnCode::new(code),
-        return_response: response,
-        return_response_url: None,
-    })
-    .collect()
 }

src/analysis/installers/burn/mod.rs
@@ -247,9 +247,8 @@ impl Installers for Burn {
                     (variable.id() == "InstallFolder").then(|| variable.resolved_value())?
                 })
                 .filter(|value| !value.contains(['[', ']']))
-                .map(|install_folder| InstallationMetadata {
-                    default_install_location: Some(Utf8PathBuf::from(&install_folder)),
-                    ..InstallationMetadata::default()
+                .map(|install_folder| {
+                    InstallationMetadata::new_install_location(Utf8PathBuf::from(&install_folder))
                 })
                 .unwrap_or_default(),
             ..Installer::default()

src/analysis/installers/inno/mod.rs
@@ -1,4 +1,3 @@
-use camino::Utf8PathBuf;
 use const_format::formatcp;
 use inno::{
     Inno,
@@ -9,9 +8,8 @@ use winget_types::{
     LanguageTag, Sha256String,
     installer::{
         AppsAndFeaturesEntries, AppsAndFeaturesEntry, Architecture as WingetArchitecture,
-        ElevationRequirement, InstallationMetadata, Installer, InstallerType, Scope,
-        UnsupportedOSArchitecture,
-        switches::{CustomSwitch, InstallerSwitches},
+        ElevationRequirement, InstallationMetadata, Installer, InstallerType, Scope, Switches,
+        UnsupportedOSArchitecture, switches::CustomSwitch,
     },
     url::DecodedUrl,
 };
@@ -91,11 +89,10 @@ impl Installers for Inno {
                 .header
                 .privileges_required()
                 .to_elevation_requirement(self.header.privileges_required_overrides_allowed()),
-            installation_metadata: InstallationMetadata {
-                default_install_location: install_dir.map(Utf8PathBuf::from),
-                ..InstallationMetadata::default()
-            },
-            ..Default::default()
+            installation_metadata: InstallationMetadata::new_install_location(
+                install_dir.map(winget_types::PathBuf::from),
+            ),
+            ..Installer::default()
         };
 
         if self
@@ -120,26 +117,24 @@ impl Installers for Inno {
                 Scope::User => (CustomSwitch::current_user(), CustomSwitch::all_users()),
             };
 
-            installer.switches = InstallerSwitches::builder()
+            installer.switches = Switches::builder()
                 .maybe_custom(has_scope_switch.then_some(default_switch))
                 .build();
 
             let override_installer = Installer {
                 scope: Some(override_scope),
-                switches: InstallerSwitches::builder()
+                switches: Switches::builder()
                     .maybe_custom(has_scope_switch.then_some(override_switch))
                     .build(),
-                installation_metadata: InstallationMetadata {
-                    default_install_location: self
-                        .header
+                installation_metadata: InstallationMetadata::new_install_location(
+                    self.header
                         .default_dir_name()
                         .map(|install_dir| {
                             to_relative_install_dir(install_dir.to_owned(), override_scope)
                         })
                         .filter(|dir| !dir.contains(['{', '}']))
-                        .map(Utf8PathBuf::from),
-                    ..InstallationMetadata::default()
-                },
+                        .map(winget_types::PathBuf::from),
+                ),
                 ..installer.clone()
             };
 

src/analysis/installers/msi/mod.rs
@@ -13,7 +13,7 @@ use winget_types::{
     LanguageTag,
     installer::{
         AppsAndFeaturesEntries, AppsAndFeaturesEntry, Architecture, InstallationMetadata,
-        Installer, InstallerSwitches, InstallerType, Scope,
+        Installer, InstallerType, Scope, Switches,
     },
 };
 
@@ -228,16 +228,12 @@ impl Msi {
         self.property_table.get(WIX_UI_INSTALL_DIR)
     }
 
-    fn install_location_switches(&self) -> InstallerSwitches {
+    fn install_location_switches(&self) -> Switches {
         self.wix_ui_install_dir()
             .and_then(|install_dir| {
                 format!(r#"{install_dir}="<INSTALLPATH>""#)
                     .parse()
-                    .map(|switch| {
-                        InstallerSwitches::builder()
-                            .install_location(switch)
-                            .build()
-                    })
+                    .map(|switch| Switches::builder().install_location(switch).build())
                     .ok()
             })
             .unwrap_or_default()
@@ -296,10 +292,9 @@ impl Installers for Msi {
             } else {
                 AppsAndFeaturesEntries::new()
             },
-            installation_metadata: InstallationMetadata {
-                default_install_location: self.find_install_directory(),
-                ..InstallationMetadata::default()
-            },
+            installation_metadata: InstallationMetadata::new_install_location(
+                self.find_install_directory(),
+            ),
             ..Installer::default()
         };
 

commit d703c68ab3f1c69bc193470da6afd2711c0599f7

Deduplicate ARP entries on update version
The removal of this was a regression from https://github.com/russellbanks/Komac/commit/44866a8326f32e8d026c56eb7d6a0dec40aacafd

src/commands/update_version.rs
@@ -133,6 +133,13 @@ impl UpdateVersion {
             .into_values()
             .flat_map(Analyzer::into_installers)
             .collect();
+
+        manifests.installer
+            .installers
+            .iter_mut()
+            .flat_map(|installer| &mut installer.apps_and_features_entries)
+            .for_each(|entry| entry.deduplicate(&manifests.default_locale));
+
         manifests.installer.optimize();
 
         manifests.update(